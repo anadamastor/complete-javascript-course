c8f5995785fdcbe3edf705a888a56366c7c12e5f
Ervis Lapi <lervis87@gmail.com>
2022-07-04 12:28:56 +0100

    first lesson

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,4 @@
+let country = "Italy";
+let contintnet = "Europe";
+let population = 100;
+console.log(country, contintnet, population);
---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,44 @@
+/*
+console.log(40 + 20 - 1);
+let js = "amazing";
+// if (js === "amazing") alert("Javascript is fun");
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "james";
+console.log(firstName);
+let javaScript = true;
+console.log(javaScript);
+
+console.log(typeof "javaScript");
+javaScript = "ciaone";
+console.log(javaScript);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+console.log(typeof null);
+
+
+// LET CONST AND VAR
+// LET for variables that will change later (mutating the var)
+let age = 30;
+age = 21;
+
+//COST is block scoped
+const birthYear = 2323;
+// birthYear = 1142;
+//Assignment to constant variable.    at script.js:30:11
+
+//VAR prior to ES 6 var declaration (function scoped)
+var job = "hello";
+job = "teacher";
+*/
+
+// BASIC OPERATORS
+const now = 2032;
+const ageJonas = now - 1991;
+const ageSarah = now - 3214;
+console.log("jonah is " + ageJonas + " years old", ageSarah ** 3);
