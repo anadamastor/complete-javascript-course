ec3d41940f8b317cb6fecd0f4bef215ac894c4d2
Ervis Lapi <lervis87@gmail.com>
2022-11-23 17:09:31 +0000

    looping over objects

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -223,3 +223,241 @@ add(3, 4, 5, 33, 5, 2);
 
 const xx = [23, 5, 7];
 add(...xx); //SBAM = THESE WILL BE UNPACKED AND PACKED THEN ABOVE
+
+// SHORT CIRCUITING
+console.log(3 || 'carl'); // > 3
+console.log('' || 'carl'); // > Carl, ""
+console.log(true || 0); // > true
+console.log(undefined || null); // > undefined is falsy
+
+console.log(undefined || 0 || '' || 'hello' || 23 || null);
+// > hello is truthy and will short circuit the whole expression. No need to look for other values.
+
+// restaurant.numGuests = 23;
+// const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
+// console.log(guests1);
+
+// setting default values
+const guests2 = restaurant.numGuests || 10;
+console.log(guests2);
+// || or operator will return the first truthy values of all operands or simply the last value if all of them are falsy
+
+// && SHORT CIRCUITING
+console.log('----AAAAAND----');
+console.log(0 && 'Jonas'); // > 0 returns the first falsy value without even considering the second one
+console.log(7 && 'Jonas'); // when it is truthy it will continue and return the last value.
+
+console.log('Hello' && 23 && null && 'jonas'); // > null
+
+// instead of creating an if condition to check if a property exists.
+if (restaurant.orderPizza) {
+  restaurant.orderPizza('mushrooms', 'spinach');
+}
+// you can short-circuit it:
+restaurant.orderPizza && restaurant.orderPizza('mushrooms', 'spinach');
+// && will return the first falsy value or the last one if all of them are truthy
+
+// && nullish coalescing CIRCUITING
+console.log('----nullish coalleshing----');
+const guestCorrect = restaurant.numGuests ?? 10;
+// works with the idea of nullish values (only null and undefined. Does not include 0 or empty string)
+
+// Logical Assignment Operators
+const rest1 = { name: 'Capri', numGuests: 20 };
+const rest2 = { name: 'La Piazza', owner: 'Giovanni Rossi' };
+
+rest1.numberGuests = rest1.numGuests || 10; // the first one will be returned if truthy, otherwise 10
+rest2.numberGuests = rest2.numGuests || 10;
+console.log(rest1, rest2);
+
+//CAN BE DNE AS BELOW:
+
+// OR ASSIGNEMENT OEPRATOR (||=)
+// rest1.numGuests ||= 10;
+// rest2.numGuests ||= 10;
+// console.log(rest1, rest2);
+// HOWEVWE IF IT IS = 0 IT WILL OVERWRITE IT
+
+// NULLISH ASSIGNEMENT OEPRATOR (??=) - NULL OR UNDEFINED
+rest1.numGuests ??= 10;
+rest2.numGuests ??= 10;
+
+// AND ASSIGNEMENT OEPRATOR (&&=) - NULL OR UNDEFINED
+
+// rest1.owner = rest1.owner && '<ANONYMOUS>';
+// rest2.owner = rest2.owner && '<ANONYMOUS>';
+
+rest1.owner &&= '<ANONYMOUS>';
+rest2.owner &&= '<ANONYMOUS>';
+// assign a variable if currently truthy.
+
+console.log(rest1, rest2);
+
+// 111- FOR-OF LOOP
+console.log('FOR OR LOOP -----------------');
+const menuForLoop = [...restaurant.starterMenu, ...restaurant.mainMenu];
+console.log(menuForLoop);
+
+// MICIDIALE - loop for arrays
+// for (const item of menuForLoop) console.log(item);
+
+// difficult to find the index of the element.(destructuring the array)
+for (const [i, el] of menuForLoop.entries()) {
+  console.log(`${i + 1}: ${el}`); // prints each time an array with index and value
+}
+
+// 112- ENHANCED OBJECT LITERALS
+console.log('ENHANCED OBJECT LITERALS -----------------');
+// OBJECT LITERAL: you write an object literal syntax. There are three ways to do so.
+
+const hoursLiteral = {
+  thu: {
+    open: 12,
+    close: 22,
+  },
+  fri: {
+    open: 11,
+    close: 23,
+  },
+  sat: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
+
+// 1 - and now to add it to an existing object you do not need to create a new key value
+const restaurantObjectLiteral = {
+  name: 'gigi',
+  hoursLiteral, // just adding it directly here
+};
+
+console.table(restaurantObjectLiteral);
+
+// 2 methods without specyfing the function keywords
+
+const restaurantObjectLiteral2 = {
+  name: 'gigi',
+  hoursLiteral, // just adding it directly here,
+  order(starterIndex, mainIndex) {
+    return 'CIAO';
+  },
+};
+
+console.log(restaurantObjectLiteral2.order());
+
+// 3 dynamic key naming: wrap it with []
+const weeKDays2 = ['mon', 'tue', 'wed', 'thu'];
+const hoursLiteral2 = {
+  name: 'gigi',
+  [weeKDays2[1]]: {
+    open: 12,
+    close: 22,
+  },
+  [weeKDays2[2]]: {
+    open: 11,
+    close: 23,
+  },
+  [`day-${2 + 4}`]: {
+    // you can also interpolate stuff
+    open: 0,
+    close: 24,
+  },
+};
+console.table(hoursLiteral2);
+
+// ----------------------------------
+// 113 - OPTIONAL CHAINING
+// ----------------------------------
+
+console.log('OPTIONAL CHAINING -----------------');
+
+// console.log(restaurant.openingHours.mon.open);
+// the above will crash script.js:374 Uncaught TypeError: Cannot read properties of undefined (reading 'open')
+
+// .mon does not exist - hence we need this to check if the property exist.
+// if (restaurant.openingHours && restaurant.openingHours.mon)
+// console.log(restaurant.openingHours.mon);
+
+// with optional chaining if a proeprty does not exist it will return underfined.
+// console.log(restaurant.openingHours.mon?.open); // only if .mon property exists then return open.
+// > undefined
+// you can create multiple chaining as well.
+// console.log(restaurant.openingHours?.mon?.open);
+
+// const daysss = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+// for (const day of daysss) {
+//   console.log(day);
+//   console.log(restaurant.openingHours[day]?.open);
+//   // brackets to read that property
+// }
+
+const daysss = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+for (const day of daysss) {
+  open = restaurant.openingHours[day]?.open ?? 'closed';
+  // ?? nullish coalescient operator takes into account the 0 so it returns it intead of having closed on saturday
+  console.log(`On ${day}, we open at ${open}`);
+  // brackets to read that property
+}
+
+// Optional jaining on methods
+
+console.log('OPTIONAL CHAINING on methods -----------------');
+console.log(restaurant.order?.(0, 1) ?? ' Method does not exist');
+// >> (2) ['Focaccia', 'Pasta']
+
+console.log(restaurant.orderRisotto?.(0, 1) ?? ' Method does not exist');
+// >>  Method does not exist
+
+// Optional chaining on arrays
+const users = [{ name: 'jonas', email: 'hello@jonas.op' }];
+console.log(users[0]?.name ?? 'user array empty');
+// >> jonas
+console.log(users[2]?.name ?? 'user array empty');
+// >> user array empty
+
+// ----------------------------------
+// 114 - LOOPING OVER OBJECTS (NOT ITERABLES)
+// ----------------------------------
+console.log('114 - LOOPING OVER OBJECTS (NOT ITERABLES-----------------');
+const openingHours = {
+  [daysss[1]]: {
+    open: 12,
+    close: 22,
+  },
+  [daysss[4]]: {
+    open: 11,
+    close: 23,
+  },
+  [daysss[5]]: {
+    // you can also interpolate stuff
+    open: 0,
+    close: 24,
+  },
+};
+
+// property Names (keys)
+const properties = Object.keys(openingHours);
+console.log(properties);
+// >> (3) ['tue', 'fri', 'sat']
+
+let openStr = `We are open on days: `;
+
+for (const day of Object.keys(openingHours)) {
+  openStr += `${day}, `;
+}
+console.log(openStr);
+// >> We are open on days: tue, fri, sat,
+
+// Property values
+const values = Object.values(openingHours);
+console.log(values);
+// >> (3) [{…}, {…}, {…}]
+
+// Entire object loop
+const entries = Object.entries(openingHours);
+console.log(entries); // transform object in
+
+// looping while destructuring
+for (const [key, { open, close }] of entries) {
+  console.log(`On ${key} we open at ${open} and close at ${close}`);
+}
