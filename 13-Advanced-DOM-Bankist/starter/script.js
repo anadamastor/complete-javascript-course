4395eea8e4444f725b9ee712a78b35ee43b8cbb1
Ervis Lapi <lervis87@gmail.com>
2022-12-28 20:43:13 +0100

    how dowm works

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -8,7 +8,8 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
-const openModal = function () {
+const openModal = function (e) {
+  e.preventDefault(); // otherwise will move page on top
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
@@ -18,8 +19,8 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
+// iterate through all of the butns to trigger the modal.
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
@@ -29,3 +30,61 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+// ====================================================================
+console.log('185. How the DOM Really Works');
+// ====================================================================
+
+// ====================================================================
+console.log('186. Selecting, Creating, and Deleting Elements');
+// ====================================================================
+console.log('187. Styles, Attributes and Classes');
+
+// ====================================================================
+console.log('188. Implementing Smooth Scrolling');
+// ====================================================================
+console.log('189. Types of Events and Event Handlers');
+
+// ====================================================================
+console.log('190. Event Propagation: Bubbling and Capturing');
+
+// ====================================================================
+console.log('191. Event Propagation in Practice');
+// ====================================================================
+console.log('192. Event Delegation: Implementing Page Navigation');
+
+// ====================================================================
+console.log('193. DOM Traversing');
+
+// ====================================================================
+console.log('194. Building a Tabbed Component');
+
+// ====================================================================
+console.log('195. Passing Arguments to Event Handlers');
+
+// ====================================================================
+console.log('196. Implementing a Sticky Navigation: The Scroll Event');
+
+// ====================================================================
+console.log('197. A Better Way: The Intersection Observer API');
+
+// ====================================================================
+console.log('198. Revealing Elements on Scroll');
+
+// ====================================================================
+console.log('199. Lazy Loading Images');
+
+// ====================================================================
+
+// ====================================================================
+console.log('00. Building a Slider Component: Part 1');
+// ====================================================================
+console.log('1. Building a Slider Component: Part 2');
+
+// ====================================================================
+console.log('202. Lifecycle DOM Events');
+
+// ====================================================================
+console.log('203. Efficient Script Loading: defer and async');
+
+// ====================================================================
