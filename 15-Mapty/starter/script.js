01c1576b1345e59a75fd3c4cb96ebbf428951108
Ervis Lapi <lervis87@gmail.com>
2023-02-08 08:07:22 +0000

    updates forms

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -11,6 +11,7 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+let map, mapEvent;
 // Geolocation api to access the position
 
 if (navigator.geolocation) {
@@ -23,8 +24,25 @@ if (navigator.geolocation) {
         `https://www.google.com/maps/search/?api=1&query=${latitude},${longitude}`
       );
       const coords = [latitude, longitude];
-      setMap(coords);
+      map = L.map('map').setView(coords, 20);
+
+      // DISPLAY MARKER
+      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution:
+          '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+      }).addTo(map);
+
+      // show workout form on click
+      map.on('click', function (mapE) {
+        // saving the global variable to have the coords later
+
+        mapEvent = mapE;
+        form.classList.remove('hidden');
+        inputDistance.focus();
+      });
     },
+
     //GeolocationPosition {coords: GeolocationCoordinates, timestamp: 1675786568060}
     function () {
       alert('couldnot get your position');
@@ -33,24 +51,37 @@ if (navigator.geolocation) {
   );
 }
 
-// LEAFLET MAP
-const setMap = function (coords) {
-  console.log('HEYYY');
+// this is a form, enter key triggers a submit event
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
 
-  const map = L.map('map').setView(coords, 20);
+  // clear input fields
+  resetInputs();
 
-  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution:
-      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-  }).addTo(map);
+  const newPopup = L.popup({
+    autoClose: false,
+    closeOnClick: null,
+    opacity: 0.4,
+    maxWidth: 250,
+    minWidth: 100,
+    className: 'running-popup',
+  }).setContent('Michi puzza');
 
-  var marker = L.marker(coords).addTo(map);
+  L.marker(mapEvent.latlng).addTo(map).bindPopup(newPopup).openPopup();
+});
 
-  map.on('click', function (e) {
-    var latlng = map.mouseEventToLatLng(e.originalEvent);
-    console.log(latlng.lat + ', ' + latlng.lng);
-    const coords = [latlng.lat, latlng.lng];
-    L.marker(coords).addTo(map);
-  });
+// changing the input form in case of cycling
+inputType.addEventListener('change', function () {
+  // selecting parent
+  inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+  inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+});
+
+const resetInputs = function () {
+  // clear input fields
+  inputCadence.valuee =
+    inputDistance.value =
+    inputCadence.value =
+    inputElevation.value =
+      '';
 };
